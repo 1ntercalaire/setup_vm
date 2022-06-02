ad4b61323c2fb5eac3f484619acd8e9dde801af6
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2022-06-02 14:07:39 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -37,3 +37,4 @@ sudo apt install ansible -y
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 sudo apt-get update && sudo apt-get install vagrant
+vagrant autocomplete install --bash --zsh
