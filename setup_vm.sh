59a0bcfb3087441ee5f5b0bd14c3cb6ef90f4a18
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2022-06-02 14:05:51 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -1,9 +1,9 @@
-#zsh
+### ZSH ###
 apt install zsh curl sudo git -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
-#docker
+### DOCKER ###
 sudo apt-get update && sudo apt-get install ca-certificates curl gnupg lsb-release openssh-server sudo -y
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -11,12 +11,12 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.
 sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 
-#docker-compose
+### DOCKER-COMPOSE ###
 curl -L https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
-#kubectl
+### KUBECTL ###
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
@@ -26,8 +26,14 @@ sudo apt-get install -y kubectl
 echo $(source <(kubectl completion zsh)) >> ~/.zshrc
 
 
-#ansible
+### ANSIBLE ###
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main"
 sudo apt update
 sudo apt install ansible -y
+
+
+### VAGRANT ###
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install vagrant
