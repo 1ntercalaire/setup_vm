3c1c6fd4d9125c71bca6d717cdbd6b4b63df85e3
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2022-06-09 15:12:57 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -78,3 +78,11 @@ apt install fonts-firacode -y
 sudo apt-get update
 sudo apt-get install cockpit -y
 sudo systemctl disable cockpit.socket
+
+
+### VIRTUALBOX ###
+sudo echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bullseye contrib" |
+sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt update
+sudo apt install virtualbox-6.1
