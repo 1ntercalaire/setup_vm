41143b8cf669d615eb2e099fdbdf15a02ca23d09
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2022-06-29 23:31:17 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -19,7 +19,7 @@ sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli container
 
 
 ### DOCKER-COMPOSE ###
-curl -L https://github.com/docker/compose/releases/download/v2.6.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/v2.6.1/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 
