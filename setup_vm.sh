1e2065d619ac09e5b03d2fbd90e8e6438448d3c4
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2022-08-30 20:06:34 +0200

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -33,7 +33,6 @@ sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://pack
 sudo echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl kubeadm kubelet
-sudo systemctl disable kubelet.service
 echo $(source <(kubectl completion zsh)) >> ~/.zshrc
 
 
