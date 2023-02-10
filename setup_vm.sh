d068409fa2d6c0bcdf3358fea9c5e7b4f9861a3f
1ntercalaire <101210873+1ntercalaire@users.noreply.github.com>
2023-02-10 22:01:13 +0100

    Update setup_vm.sh

--- setup_vm.sh
+++ setup_vm.sh
@@ -8,7 +8,7 @@ sudo systemctl reload sshd
 sudo apt install zsh curl sudo git -y
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sed -i "s/robbyrussell/duellj/g" .zshrc
-sed -i "s/plugins=(git)/plugins=(git docker docker-compose vagrant nmap)/g" .zshrc
+sed -i "s/plugins=(git)/plugins=(git docker docker-compose)/g" .zshrc
 sed -i 's|# export PATH=$HOME/bin:/usr/local/bin:$PATH|export PATH=$HOME/bin/:/usr/local/bin/:/usr/sbin/:$PATH|g' .zshrc
 
 
